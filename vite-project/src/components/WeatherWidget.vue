cd913db0f7b77159de17f5d0f34824adf1107e23
兮. <1269838578@qq.com>
2026-07-06 13:32:16 +0800

    支持ipv6

--- vite-project/src/components/WeatherWidget.vue
+++ vite-project/src/components/WeatherWidget.vue
@@ -50,7 +50,7 @@ const fetchWeatherData = async () => {
   `
 
   try {
-    const response = await fetch('https://api.muqingxi.com/proxy/getWeather')
+    const response = await fetch('https://muqingxi.com:2345/proxy/getWeather')
     if (!response.ok) {
       throw new Error(`HTTP错误: ${response.status}`)
     }
